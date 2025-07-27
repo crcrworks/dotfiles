bc4aba463a1e6783588f8192fd58b77c44856080
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-07-27 11:29:36 +0900

    nvim(nvim-tree): fix lefty

--- dot_config/exact_nvim/lua/plugins/nvim-tree.lua
+++ dot_config/exact_nvim/lua/plugins/nvim-tree.lua
@@ -5,7 +5,6 @@ local function righty()
 end
 
 local function lefty()
-  api.node.navigate.parent()
   local node_at_cursor = api.tree.get_node_under_cursor()
   if node_at_cursor.nodes and node_at_cursor.open then
     api.node.open.edit()

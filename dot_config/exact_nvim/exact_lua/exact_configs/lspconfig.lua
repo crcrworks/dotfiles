3db71b17a1fa2fdaf0c2db1a9ea4c276ebb8f7c8
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-07-27 18:31:56 +0900

    nvim: disable css format

--- dot_config/exact_nvim/exact_lua/exact_configs/lspconfig.lua
+++ dot_config/exact_nvim/exact_lua/exact_configs/lspconfig.lua
@@ -2,44 +2,11 @@ require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "biome", "rust-analyzer" }
 
--- vim.lsp.config("vtsls", {
---   filetypes = {
---     "javascript",
---     "javascriptreact",
---     "javascript.jsx",
---     "typescript",
---     "typescriptreact",
---     "typescript.tsx",
---   },
---   settings = {
---     typescript = {
---       format = false,
---       updateImportsOnFileMove = { enabled = "always" },
---       inlayHints = {
---         parameterNames = { enabled = "all" },
---         parameterTypes = { enabled = true },
---         variableTypes = { enabled = true },
---         propertyDeclarationTypes = { enabled = true },
---         functionLikeReturnTypes = { enabled = true },
---         enumMemberValues = { enabled = true },
---       },
---     },
---     javascript = {
---       updateImportsOnFileMove = { enabled = "always" },
---       inlayHints = {
---         parameterNames = { enabled = "literals" },
---         parameterTypes = { enabled = true },
---         variableTypes = { enabled = true },
---         propertyDeclarationTypes = { enabled = true },
---         functionLikeReturnTypes = { enabled = true },
---         enumMemberValues = { enabled = true },
---       },
---     },
---     vtsls = {
---       enableMoveToFileCodeAction = true,
---     },
---   },
--- })
+vim.lsp.config("cssls", {
+  init_options = {
+    provideFormatter = false,
+  },
+})
 
 vim.lsp.config("rust-analyzer", {
   settings = {

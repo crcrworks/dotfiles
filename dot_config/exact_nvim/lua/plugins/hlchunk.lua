190d21ec0f01b50acfb941ab3007fdd10efbe595
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-07-27 12:39:47 +0900

    nvim: replace colors referenced from theme

--- dot_config/exact_nvim/lua/plugins/hlchunk.lua
+++ dot_config/exact_nvim/lua/plugins/hlchunk.lua
@@ -1,48 +1,56 @@
+local theme = require "base46.themes.everforest"
+
 return {
-  "shellRaining/hlchunk.nvim",
-  event = { "BufRead" },
-  config = function()
-    require("hlchunk").setup {
-      ---@diagnostic disable-next-line: assign-type-mismatch
-      chunk = {
-        enable = true,
-        notify = true,
-        use_treesitter = false,
-        -- details about support_filetypes and exclude_filetypes in https://github.com/shellRaining/hlchunk.nvim/blob/main/lua/hlchunk/utils/filetype.lua
-        chars = {
-          horizontal_line = "─",
-          vertical_line = "│",
-          left_top = "╭",
-          left_bottom = "╰",
-          right_arrow = ">",
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    enabled = false,
+  },
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "BufRead" },
+    config = function()
+      require("hlchunk").setup {
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        chunk = {
+          enable = true,
+          notify = true,
+          use_treesitter = false,
+          -- details about support_filetypes and exclude_filetypes in https://github.com/shellRaining/hlchunk.nvim/blob/main/lua/hlchunk/utils/filetype.lua
+          chars = {
+            horizontal_line = "─",
+            vertical_line = "│",
+            left_top = "╭",
+            left_bottom = "╰",
+            right_arrow = ">",
+          },
+          style = {
+            { fg = theme.base_30.light_grey },
+            { fg = theme.base_30.red }, -- this fg is used to highlight wrong chunk
+          },
+          textobject = "",
+          max_file_size = 1024 * 1024,
+          error_sign = true,
+          duration = 0,
+          delay = 0,
         },
-        style = {
-          { fg = "#859289" },
-          { fg = "#e67e80" }, -- this fg is used to highlight wrong chunk
-        },
-        textobject = "",
-        max_file_size = 1024 * 1024,
-        error_sign = true,
-        duration = 0,
-        delay = 0,
-      },
 
-      indent = {
-        enable = true,
-        use_treesitter = false,
-        chars = {
-          "│",
+        indent = {
+          enable = true,
+          use_treesitter = false,
+          chars = {
+            "│",
+          },
+          style = {
+            { fg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID "Whitespace"), "fg", "gui") },
+          },
         },
-        style = {
-          { fg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID "Whitespace"), "fg", "gui") },
-        },
-      },
 
-      line_num = {
-        enable = true,
-        use_treesitter = false,
-        style = "#859289",
-      },
-    }
-  end,
+        line_num = {
+          enable = true,
+          use_treesitter = false,
+          style = theme.base_30.light_grey,
+        },
+      }
+    end,
+  },
 }
--- dot_config/exact_nvim/lua/plugins/scrollbar.lua
+++ dot_config/exact_nvim/lua/plugins/scrollbar.lua
@@ -1,31 +1,21 @@
+local theme = require "base46.themes.everforest"
+
 return {
   "petertriho/nvim-scrollbar",
   event = "BufRead",
   config = function()
-    -- gruvbox-material color pallet
-    -- https://user-images.githubusercontent.com/58662350/213884009-87533cf3-e3c5-4d46-85f7-f6993b6dd887.png
-    local colors = {
-      grey = "#374145",
-      red = "#e67e80",
-      yellow = "#dbbc7f",
-      green = "#83c092",
-      blue = "#7fbbb3",
-      orange = "#e69875",
-      purple = "#d699b6",
-    }
-
     require("scrollbar").setup {
       handle = {
         blend = 30,
-        color = colors.grey,
+        color = theme.base_30.grey,
       },
       marks = {
-        Search = { color = colors.orange },
-        Error = { color = colors.red },
-        Warn = { color = colors.yellow },
-        Info = { color = colors.purple },
-        Hint = { color = colors.green },
-        Misc = { color = colors.blue },
+        Search = { color = theme.base_30.orange },
+        Error = { color = theme.base_30.red },
+        Warn = { color = theme.base_30.yellow },
+        Info = { color = theme.base_30.purple },
+        Hint = { color = theme.base_30.green },
+        Misc = { color = theme.base_30.nord_blue },
       },
       excluded_filetypes = {
         "",

311e5ec1d30e78cf9c51d42f94e91bd38e7a5d02
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-07-29 10:23:32 +0900

    nvim: fix nvim-cmp not working

--- dot_config/exact_nvim/exact_lua/exact_plugins/nvim-cmp.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/nvim-cmp.lua
@@ -1,7 +1,64 @@
-pcall(function()
-  dofile(vim.g.base46_cache .. "syntax")
-  dofile(vim.g.base46_cache .. "treesitter")
-end)
+dofile(vim.g.base46_cache .. "cmp")
+
+local options = function()
+  local cmp = require "cmp"
+  local options = {
+    completion = { completeopt = "menu,menuone,noselect" },
+
+    snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body)
+      end,
+    },
+
+    mapping = {
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.close(),
+
+      ["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+      },
+
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif require("luasnip").expand_or_jumpable() then
+          require("luasnip").expand_or_jump()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif require("luasnip").jumpable(-1) then
+          require("luasnip").jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    },
+
+    sources = {
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+      { name = "buffer" },
+      { name = "nvim_lua" },
+      { name = "async_path" },
+    },
+    performance = {
+      max_view_entries = 7,
+    },
+  }
+
+  return vim.tbl_deep_extend("force", options, require "nvchad.cmp")
+end
 
 return {
   "hrsh7th/nvim-cmp",
@@ -44,15 +101,5 @@ return {
     },
   },
 
-  opts = {
-    completion = { completeopt = "menu,menuone,noselect" },
-    sources = {
-      { name = "nvim_lsp" },
-      { name = "buffer" },
-      { name = "luasnip" },
-    },
-    performance = {
-      max_view_entries = 7,
-    },
-  },
+  opts = options,
 }

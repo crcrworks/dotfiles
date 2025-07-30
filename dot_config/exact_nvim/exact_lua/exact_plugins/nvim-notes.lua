e70295aed639aac62fa8c760a814d42c0e0d366a
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-07-31 01:11:34 +0900

    nvim: chore: #nvim-notes setup

--- dot_config/exact_nvim/exact_lua/exact_plugins/nvim-notes.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/nvim-notes.lua
@@ -1,5 +1,18 @@
 return {
   "Samar-exe/nvim-notes",
   cmd = { "NotesNew", "NotesOpen", "NotesBrowse", "NotesList", "NotesDaily", "NotesQuick", "NotesSearch" },
-  opts = {},
+  opts = {
+    notes_dir = vim.fn.expand "~/Documents/notes/",
+    directories = {
+      "inbox", -- Quick capture
+    },
+    templates = {
+      default = {
+        "# {title}",
+        "",
+        "Created: {datetime}",
+        "",
+      },
+    },
+  },
 }

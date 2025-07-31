c8d09d7a13a1d6ccbd2f0086989d5d8e276d37e4
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-07-31 14:52:33 +0900

    ghdash: init config

---
+++ dot_config/exact_nvim/exact_lua/exact_lsp/yamlls.lua
@@ -0,0 +1,11 @@
+return {
+  "yamlls",
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://json.schemastore.org/taskfile.json"] = { "[Tt]askfile.*.yml", "[Tt]askfile.yml" },
+        ["https://dlvhdr.github.io/gh-dash/configuration/gh-dash/schema.json"] = { "gh-dash.yml" },
+      },
+    },
+  },
+}

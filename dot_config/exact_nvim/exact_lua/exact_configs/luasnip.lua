3cbe1ee325904b456609797747e70dbb3661a69e
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-08-15 13:13:35 +0900

    nvim: setup rust snippets

--- dot_config/exact_nvim/exact_lua/exact_configs/luasnip.lua
+++ dot_config/exact_nvim/exact_lua/exact_configs/luasnip.lua
@@ -11,3 +11,23 @@ require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snip
 -- lua format
 require("luasnip.loaders.from_lua").load()
 require("luasnip.loaders.from_lua").lazy_load { paths = vim.g.lua_snippets_path or "" }
+
+local ls = require "luasnip"
+
+vim.keymap.set({ "i" }, "<C-K>", function()
+  ls.expand()
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-l>", function()
+  ls.jump(1)
+end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-h>", function()
+  ls.jump(-1)
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-e>", function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end, { silent = true })
+
+return {}

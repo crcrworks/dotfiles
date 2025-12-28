971e24df1c1e4e8eba97c8e45b8e927612f13b03
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-12-28 12:13:33 +0900

    nvim: setup rust for code_action

--- dot_config/exact_nvim/exact_lua/exact_plugins/code-action-menu.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/code-action-menu.lua
@@ -12,5 +12,18 @@ return {
   event = "LspAttach",
   opts = {
     picker = "snacks",
+    sort = function(a, b)
+      -- Prioritize actions from rust_analyzer
+      if a.client.name == "rust_analyzer" and b.client.name ~= "rust_analyzer" then
+        return true
+      elseif a.client.name ~= "rust_analyzer" and b.client.name == "rust_analyzer" then
+        return false
+      end
+
+      -- Sort by action kind alphabetically
+      local a_kind = a.action.kind or ""
+      local b_kind = b.action.kind or ""
+      return a_kind < b_kind
+    end,
   },
 }

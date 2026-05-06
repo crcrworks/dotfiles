bcb8a31bd66f7cee6f72d396a67eae1b7a07cbcc
crcrworks <42961885+crcrworks@users.noreply.github.com>
2026-05-06 21:08:51 +0900

    update

--- dot_config/exact_nvim_astro/lua/plugins/astrolsp.lua
+++ dot_config/exact_nvim_astro/lua/plugins/astrolsp.lua
@@ -21,6 +21,8 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
+        -- Let Biome (conform, etc.) handle Svelte formatting; avoid double-format with svelte LSP.
+        "svelte",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function

c298b24ce236d925af3877267139f2b20109c2ed
crcrworks <42961885+crcrworks@users.noreply.github.com>
2026-05-06 14:30:08 +0900

    update

--- dot_config/exact_nvim_astro/lua/plugins/astrocore.lua
+++ dot_config/exact_nvim_astro/lua/plugins/astrocore.lua
@@ -3,9 +3,8 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
-    -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      large_buf = { size = 1024 * 256, lines = 10000 },
       autopairs = true,
       cmp = true,
       diagnostics = { virtual_text = true, virtual_lines = false },
@@ -16,9 +15,7 @@ return {
       virtual_text = true,
       underline = true,
     },
-    -- passed to `vim.filetype.add`
     filetypes = {
-      -- see `:h vim.filetype.add` for usage
       extension = {
         foo = "fooscript",
       },
@@ -31,18 +28,14 @@ return {
     },
     options = {
       opt = {
-        relativenumber = true,
+        relativenumber = false,
         number = true,
         spell = false,
         signcolumn = "yes",
         wrap = false,
       },
-      g = {
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
-      },
+      g = {},
     },
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
       n = {
         ["<Tab>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },

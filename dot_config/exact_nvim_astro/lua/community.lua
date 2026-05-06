5e8f1d30cd0277c3ae21d21c1345f7cbdd42bd7b
crcrworks <42961885+crcrworks@users.noreply.github.com>
2026-05-06 14:54:38 +0900

    update

--- dot_config/exact_nvim_astro/lua/community.lua
+++ dot_config/exact_nvim_astro/lua/community.lua
@@ -1,8 +1,18 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  -- { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.biome" },
+  { import = "astrocommunity.pack.typescript" },
+
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+
+  { import = "astrocommunity.recipes.picker-nvchad-theme" },
+  { import = "astrocommunity.recipes.disable-borders" },
 
   { import = "astrocommunity.scrolling.nvim-scrollbar" },
 

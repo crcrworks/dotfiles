c74643e8ac29400ee58e40387883ee4405e44550
crcrworks <42961885+crcrworks@users.noreply.github.com>
2026-05-13 13:13:29 +0900

    feat: add json-language-server support for exact_lua plugin in dot_config/exact_nvim/exact_lua/exact_plugins

--- dot_config/exact_nvim/exact_lua/exact_plugins/exact_lang/init.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/exact_lang/init.lua
@@ -2,6 +2,7 @@ return {
   { import = "plugins.lang.biome" },
   { import = "plugins.lang.go" },
   { import = "plugins.lang.html-css" },
+  { import = "plugins.lang.json" },
   { import = "plugins.lang.lua" },
   { import = "plugins.lang.python" },
   { import = "plugins.lang.rust" },
